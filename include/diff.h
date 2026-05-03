35a5b4db42e1f41306fccc5fdffee64d9c5f41c5
castrfy <hasanknk2317@gmail.com>
2026-05-03 19:35:38 +0300

    uptaded diff implementation

--- include/diff.h
+++ include/diff.h
@@ -41,10 +41,12 @@ bool calculate_diff(std::string filename1, std::string filename2)
 
 
     //differencing
-    std::vector<size_t> diff;
+    std::vector<size_t> created; //index on file2
+    std::vector<size_t> removed; //index on file1
 
     int line_diff = data1.size() - data2.size();
-    int diff_count = 0;
+    int create_count = 0;
+    int removed_count = 0;
     std::cout << "Dosya satır sayısı farkı: " << line_diff << std::endl;
     if (line_diff == 0)
     {
@@ -52,82 +54,162 @@ bool calculate_diff(std::string filename1, std::string filename2)
         {
             if (data1[index] != data2[index])
             {
-                diff.push_back(index);
+                removed.push_back(index);
+                removed_count++;
+                created.push_back(index);
+                create_count++;
             }
         }
         
     }
-    else
+    else if (line_diff < 0) //data2 büyük
     {
-        if (line_diff < 0) //data2 büyük
-        {
-            for (size_t i = 0; i < data1.size(); i++)
+        std::vector<int> shifted(data1.size(), 0);
+        
+        // çıkarılanlar
+        size_t found_i = -1;
+        for (size_t k = 1; k <= -line_diff; k++)
+        {   
+            if (data1[0] == data2[0])
             {
-                if (data1[i] != data2[i])
+                found_i = 0;
+                break;
+            }
+            if (data1[0] == data2[0 + k])
+            {
+                found_i = k;
+                break;
+            }
+        }
+        if (found_i == -1)
+        {
+            shifted[0] = -1;
+            removed.push_back(0);
+            removed_count++;
+        }
+        else shifted[0] = found_i;
+        
+        for (size_t i = 1; i < data1.size(); i++)
+        {
+            if (shifted[i-1] != -1 && data1[i] == data2[i + shifted[i-1]])
+            {
+                shifted[i] = shifted[i-1];
+            }
+            else if (data1[i] != data2[i])
+            {
+                for (size_t k = 1; k <= -line_diff; k++)
                 {
-                    if (data1[i - (diff_count + line_diff)] != data2[i])
-                        {
-                            if (data1[i - diff_count] != data2[i])
-                            {
-                                diff.push_back(i);
-                                diff_count++;
-                            }
-                        }
+                    found_i = -1;
+                    if (data1[i] == data2[i + k])
+                    {
+                        found_i = k;
+                        break;
+                    }
+                }
+                if (found_i == -1) 
+                {
+                    shifted[i] = -1;
+                    removed.push_back(i);
+                    removed_count++;
                 }
                 else
                 {
-                    // aynı satır eşit ama önceki satırın klonu olabilir, diff_count
+                    shifted[i] = found_i;
                 }
-            }
-            for (size_t i = data2.size() - diff_count -2 ; i < data2.size(); i++)
-            {
-                
-                if (data1[i] != data2[i])
-                {
-                    if (data1[i - (diff_count + line_diff)] != data2[i])
-                        {
-                            if (data1[i - diff_count] != data2[i])
-                            {
-                                diff.push_back(i);
-                                diff_count++;
-                            }
-                        }
-                }
-                
             }
         }
-        else //data1 büyük
+        
+        for (size_t i = data1.size(); i < data2.size(); i++)
         {
-            for (size_t i = 0; i < data2.size(); i++)
+            if (data1[i - shifted[i-1]] == data2[i])
             {
-                if (data2[i] != data1[i])
-                    if (data2[i - diff_count] != data1[i])
-                    {
-                        diff.push_back(i);
-                        diff_count++;
-                    }
-                
+                shifted[i] = shifted[i-1];
             }
-            for (size_t i = data1.size() - diff_count - 1 ; i < data1.size(); i++)
+            else if (data1[i] != data2[i])
             {
-                
-                if (data2[i - diff_count] != data1[i])
+                for (size_t k = 1; k <= -line_diff; k++)
                 {
-                    diff.push_back(i);
-                    diff_count++;
+                    found_i = -1;
+                    if (data1[i] == data2[i + k])
+                    {
+                        found_i = k;
+                        break;
+                    }
+                }
+                if (found_i == -1) 
+                {
+                    shifted[i] = -1;
+                    removed.push_back(i);
+                    removed_count++;
+                }
+                else
+                {
+                    shifted[i] = found_i;
+                }
+            }
+        }
+        
+
+
+        for (size_t i = 0; i < shifted.size(); i++)
+        {
+            std::cout << "[" << i << "] " << shifted[i] << std::endl;
+        }
+        
+        
+        // eklenenler
+        for (size_t i = 0; i < data2.size(); i++)
+        {
+            if (data1[i] != data2[i])
+            {
+                if (data1[i - shifted[i]] != data2[i])
+                {
+                    created.push_back(i);
+                    create_count++;
                 }
                 
             }
+            
+        }
+
+    }
+    else //data1 büyük
+    {
+        for (size_t i = 0; i < data2.size(); i++)
+        {
+            if (data2[i] != data1[i])
+                if (data2[i - create_count] != data1[i])
+                {
+                    created.push_back(i);
+                    create_count++;
+                }
+            
+        }
+        for (size_t i = data1.size() - create_count - 1 ; i < data1.size(); i++)
+        {
+            
+            if (data2[i - create_count] != data1[i])
+            {
+                created.push_back(i);
+                create_count++;
+            }
+            
         }
     }
+    
 
     std::string output1;
     std::string output2;
     
-    std::cout << diff_count << " satırda fark bulundu" << std::endl;
-    for (size_t i : diff)
+    std::cout << create_count << " satır eklendi" << std::endl;
+    std::cout << removed_count << " satır çıkarıldı" << std::endl;
+    for (size_t i : created)
     {
-        std::cout << "Yeni Satır " << i + 1 << ":" << std::endl << (line_diff < 0 ? data2[i] : data1[i]) << std::endl;
+        std::cout << "+ [" << i + 1 << "]:" << (line_diff < 0 ? data2[i] : data1[i]) << std::endl;
+    }
+    for (size_t i : removed)
+    {
+        std::cout << "- [" << i + 1 << "]:" << (line_diff < 0 ? data1[i] : data2[i]) << std::endl;
     }
     
     return true;
